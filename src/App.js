c9166c45c0eaf55982317f403ae994372123dfc4
Hariom1711 <2003om1711@gmail.com>
2023-03-15 10:03:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Cardsdetails from './components/Cardsdetails';
+import Cards from './components/Cards';
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart/:id' element={<Cardsdetails />} />
+   </Routes>
+  </>
   );
 }
 
