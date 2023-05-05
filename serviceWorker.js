127549e6327429df600e97815551348a4fbe5546
Hariom Ojha <97445797+Hariom1711@users.noreply.github.com>
2023-05-05 11:07:53 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,42 +1,38 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html', 'offline.html' ];
+const CACHE_NAME = 'version-1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'cards.js',
+  'cardDetails.js',
+  'manifest.json',
+  'favicon.ico',
+  'logo192.png',
+  'logo512.png',
+  'static/css/main.5ecd60fb.chunk.css',
+  'static/js/2.2c7590c7.chunk.js',
+  'static/js/main.1ecf9a51.chunk.js'
+];
 
-const self = this;
-
-// Install Service Worker
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    )
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Listen for requests
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request)
-                    .catch(() => caches.match('offline.html'))
-            })
-    )
-});
-
-// Activate Service Worker
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)){
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-    )
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
